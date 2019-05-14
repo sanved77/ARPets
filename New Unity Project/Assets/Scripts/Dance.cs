acc7872361b4da9d59c97f3c7bc93fe3b3edcdbf
Sanved <sanved77@gmail.com>
2019-05-14 00:24:01 -0400

    final

---
+++ New Unity Project/Assets/Scripts/Dance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dance : MonoBehaviour {
+
+	public Animator anim;
+	public Button m_YourFirstButton;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		m_YourFirstButton.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TaskOnClick()
+    {
+        anim.SetBool("isFly", true);
+        Debug.Log("fkyyyy");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+	public Button m_YourFirstButton;
+	// Use this for initialization
+	void Start () {
+		m_YourFirstButton.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+}
